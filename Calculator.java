b0ad9882984fe2ed366ff73b394aaab2838e200e
JiHye <ghaee200@gmail.com>
2017-11-25 19:10:40 +0900

    Calculator interface 수정

--- Calculator.java
+++ Calculator.java
@@ -12,28 +12,35 @@ class Calculator{
 			System.out.println("5. Convert Units");
 			System.out.println("6. Quit");
 			
-			System.out.print("Enter the number of your choice: ");
+			System.out.print("Select: ");
 			select = scan.nextInt();
 			switch(select){
 			case 1:
+				System.out.println("----------------------------");
 				System.out.print("Enter 2 numbers: ");
 				double add_n1 = scan.nextDouble();
 				double add_n2 = scan.nextDouble();
 				System.out.println(add_n1 + " + " + add_n2 + " = "+add(add_n1,add_n2));
+				System.out.println("----------------------------");
 				break;
 			case 2:
+				System.out.println("----------------------------");
 				System.out.print("Enter 2 numbers: ");
 				double sub_n1 = scan.nextDouble();
 				double sub_n2 = scan.nextDouble();
 				System.out.println(sub_n1 + " - " + sub_n2 + " = "+subtract(sub_n1,sub_n2));
+				System.out.println("----------------------------");
 				break;
 			case 3:
+				System.out.println("----------------------------");
 				System.out.print("Enter 2 numbers: ");
 				double mulpy_n1 = scan.nextDouble();
 				double mulpy_n2 = scan.nextDouble();
 				System.out.println(mulpy_n1 + " x " + mulpy_n2 + " = "+multiply(mulpy_n1,mulpy_n2));
+				System.out.println("----------------------------");
 				break;
 			case 4:
+				System.out.println("----------------------------");
 				System.out.print("Enter 2 numbers: ");
 				double div_n1 = scan.nextDouble();
 				double div_n2 = scan.nextDouble();
@@ -44,8 +51,10 @@ class Calculator{
 					div_n2 = scan.nextDouble();
 				}
 				System.out.println(div_n1 + " / " + div_n2 + " = "+divide(div_n1,div_n2));
+				System.out.println("----------------------------");
 				break;
 			case 5:
+				System.out.println("----------------------------");
 				System.out.print("value:");
 				double convert_n = scan.nextDouble();
 				System.out.print("unit (pound,kg,inch,cm,F,C): ");
@@ -53,7 +62,7 @@ class Calculator{
 				convert(convert_n,unit);
 				break;
 			case 6:
-				System.out.println("Terminate Calculator");
+				System.out.println("Go to main menu");
 				return;
 			default:
 				System.out.println("Please check your number");
@@ -84,28 +93,40 @@ class Calculator{
 		// TODO Auto-generated method stub
 		switch(unit){
 		case "pound":
+			System.out.println("----------------------------");
 			double kg_value = value * 0.453592; // pound -> kg
 			System.out.println(value+ " pound" + " -> " + kg_value + " kg" );
+			System.out.println("----------------------------");
 			break;
 		case "kg": //kg -> pound
+			System.out.println("----------------------------");
 			double pound_value = value * 2.204622; // kg -> pound
 			System.out.println(value+ " kg" + " -> " + pound_value + " pound" );
+			System.out.println("----------------------------");
 			break;
 		case "inch":
+			System.out.println("----------------------------");
 			double cm_value = value * 2.54; // inch -> cm
 			System.out.println(value+ " inch" + " -> " + cm_value + " cm" );
+			System.out.println("----------------------------");
 			break;
 		case "cm":
+			System.out.println("----------------------------");
 			double inch_value = value / 2.54; // cm -> inch
 			System.out.println(value+ " cm" + " -> " + inch_value + " inch" );
+			System.out.println("----------------------------");
 			break;
 		case "F":
+			System.out.println("----------------------------");
 			double C_value = ((value-32)*5)/9 ; // F -> C
 			System.out.println(value+ " F" + " -> " + C_value + " C" );
+			System.out.println("----------------------------");
 			break;
 		case "C":
+			System.out.println("----------------------------");
 			double F_value = value * 9/5 +32; //C -> F
 			System.out.println(value+ "C" + "-> " + F_value + "F" );
+			System.out.println("----------------------------");
 			break;
 	
 		}
